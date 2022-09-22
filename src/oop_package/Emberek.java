86537f1272abfb348d70429555d3bd9b83a3c1ce
szoker <MY_NAME@example.com>
2022-09-22 16:40:57 +0200

    list.add

--- src/oop_package/Emberek.java
+++ src/oop_package/Emberek.java
@@ -1,8 +1,18 @@
 package oop_package;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Emberek {
-    private List<Emberek> emberek;
+    private List<ember> emberek;
+
+    public Emberek(ember[] emberTomb){
+       this.emberek = new ArrayList<>();
+       for (ember e: emberTomb){
+           emberek.add(e);
+           this.emberek.addAll(Arrays.asList(emberTomb));
+       }
+    }
 
 }
